daeb586e17e7f01700774c0f4fed38ea7dc030a6
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-02-08 15:36:11 +0100

    added film added cardFilm Completed

---
+++ vue-project/src/components/CardFilm.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+    name:"CardFilm",
+    props:{
+        propsThumb:String, 
+        propsName:String,
+    }    
+}
+</script>
+
+
+
+<template>
+
+        <div>
+        <figure>
+            <img :src= propsThumb>
+        </figure>
+            <span>{{propsName}}</span>
+        </div>
+    
+</template>
+
+<style scoped>
+
+div{
+    display: flex;
+    flex-direction: column;
+    width: calc( 100% / 6 - 20px);
+    gap: 10px;
+    figure{ 
+        img{
+            width: 100%;
+            aspect-ratio: 1;
+        }
+    }
+    span{
+        color: white;
+    }
+}
+
+</style>
